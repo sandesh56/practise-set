73980154911e57e8cc4850591773878c05985d86
Sandesh <deathrider644@gmail.com>
2019-11-22 11:18:53 +0545

    initial commit

---
+++ resources/views/test.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title> PHP with MySQL</title>
+</head>
+
+<body>
+<?php
+$retval = mysql_function(value,[value,.....]);
+if(!$retval) {
+    die("error: a reletaed errror message");
+}
+?>
+</body>
+</html>
+
